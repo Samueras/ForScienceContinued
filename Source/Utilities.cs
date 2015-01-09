03ef06678d38b1216416a1aa730aada4514e7bcc
Xarun <Xarun@users.noreply.github.com>
2015-01-09 20:30:23 +0100

    reintegrated experimentlimit check+change helper class to utilities

---
+++ Source/Utilities.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace ForScience
+{
+  public static class Utilities
+  {
+    public static void LogDebugMessage(string message, params string[] strings)//thanks Sephiroth018 for this part
+    {
+#if DEBUG
+      Debug.Log(string.Format("[For Science] " + message, strings));
+#endif
+    }
+    public static void clampToScreen(ref Rect rect)
+    {
+      rect.x = Mathf.Clamp(rect.x, 0, Screen.width - rect.width);
+      rect.y = Mathf.Clamp(rect.y, 0, Screen.height - rect.height);
+    }
+  }
+}
